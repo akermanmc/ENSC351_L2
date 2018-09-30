9dfbeacd9d358b1c311774699ce29d153a8e6830
akermanmc <43158709+akermanmc@users.noreply.github.com>
2018-09-30 14:10:28 -0700

    Add files via upload

---
+++ concert_locksM2.cpp
@@ -0,0 +1,36 @@
+// ENSC 351 Lab 2
+// implement 3 locks
+
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+// method 2: array of 10,000 boolean variables
+
+void* spin2(void* val){
+	//cout << "in spin" << endl;
+	
+	int* thread_cond = (int*)val; //cast void parameter into int for while condition
+	while(*thread_cond) {}
+}
+
+int main(){
+	//create 1000 pthreads - all spin on one variable
+	int all_threads_are_created = 1; // all threads initially spin on this variable
+	pthread_t thread_num[1000] = {0}; // array of pthread identifiers
+
+	for (int i=0;i<1000;i++)
+	{
+		pthread_create(&thread_num[i], NULL, spin2, &all_threads_are_created);
+	}
+
+	//release all threads simultaneously:
+	all_threads_are_created = 0;
+
+
+
+	return 0;
+}
---
+++ concert_locksM3.cpp
@@ -0,0 +1,34 @@
+// ENSC 351 Lab 2
+// implement 3 locks
+
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+// method 3: single ticket lock
+
+void* spin3(void* val){
+	//cout << "in spin" << endl;
+	
+	int* thread_cond = (int*)val; //cast void parameter into int for while condition
+	while(*thread_cond) {}
+}
+
+int main(){
+	//create 1000 pthreads - all spin on one variable
+	int all_threads_are_created = 1; // all threads initially spin on this variable
+	pthread_t thread_num[1000] = {0}; // array of pthread identifiers
+
+	for (int i=0;i<1000;i++)
+	{
+		pthread_create(&thread_num[i], NULL, spin1, &all_threads_are_created);
+	}
+
+	//release all threads simultaneously:
+	all_threads_are_created = 0;
+
+	return 0;
+}
