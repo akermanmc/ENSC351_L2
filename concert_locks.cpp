ab184c953b80696b2d15cf028d298a619bb41d93
akermanmc <43158709+akermanmc@users.noreply.github.com>
2018-09-30 13:10:45 -0700

    creates 1000 pthreads and implements the first...
    method (single pthread_mutex_lock)

--- concert_locks.cpp
+++ concert_locks.cpp
@@ -7,31 +7,54 @@
 #include <unistd.h>
 
 using namespace std;
+int counter = 0;
+pthread_mutex_t lock;
 
-// initial function for pthread creation
-void* init_spin(void* val)
-{
+
+// method 1: single pthread_lock
+// can't find pthread_lock... use pthread_mutex_lock?
+void* spin1(void* val){
 	//cout << "in spin" << endl;
-	int* thread_cond = (int*)val;
+	
+	int* thread_cond = (int*)val; //cast void parameter into int for while condition
 	while(*thread_cond) {}
+
+	pthread_mutex_lock(&lock);
+	counter +=1;
+	//cout << counter << endl;
+	pthread_mutex_unlock(&lock);
 }
+
+
+// method 2: array of 10,000 boolean variables
+// -- code -- 
+
+// method 3: single ticket lock
+// -- code -- 
+
 
 int main(){
 	//create 1000 pthreads - all spin on one variable
 	int all_threads_are_created = 1; // all threads initially spin on this variable
 	pthread_t thread_num[1000] = {0}; // array of pthread identifiers
 
+	pthread_mutex_init(&lock, NULL);
+
 	for (int i=0;i<1000;i++)
 	{
-		pthread_create(&thread_num[i], NULL, init_spin, &all_threads_are_created);
+		pthread_create(&thread_num[i], NULL, spin1, &all_threads_are_created);
 	}
+
+	//release all threads simultaneously:
+	all_threads_are_created = 0;
+
+	// wait for threads to pass through counter
+	cout << counter << endl;
+	while (counter < 1000){}
+	cout << counter << endl;
+
+
 	return 0;
 }
 
-// method 1: single pthread_lock
 
-
-// method 2: array of 10,000 boolean variables
-
-
-// method 3: single ticket lock
